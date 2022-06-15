6290d0c558e45e8fd7add9e3ba02da1051b10729
Santi F <santiagofilgueira02@gmail.com>
2022-06-14 21:15:05 -0300

    firebase

--- src/components/ItemListContainer/index.jsx
+++ src/components/ItemListContainer/index.jsx
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react' ;
 import ItemList from '../ItemList';
 import Title from '../Title';
+// import { getFirestore, getDoc, getDocs, setItems, collection, doc} from "firebase/firestore";
 
 const films = [
     { id: 1, price: 10000, image: "https://www.cataloniahotels.com/es/blog/wp-content/uploads/2016/05/habitaci%C3%B3n-individual-catalonia-620x412.jpg" , title: "Single" },
@@ -8,10 +9,8 @@ const films = [
     { id: 3, price: 19750, image:"https://i.pinimg.com/originals/08/a7/dd/08a7dd8746cac9afed89b25265871dc2.jpg", title: "Suite" },
 ];
 
-
 export const ItemListContainer = ({texto}) => {
-    
-    
+
     const [data, setData] = useState([]);
 
     useEffect(() => {
@@ -23,6 +22,34 @@ export const ItemListContainer = ({texto}) => {
         getData.then(res => setData(res));
 
     }, [])
+
+    // useEffect(() => {
+    //     const db = getFirestore()
+
+    //     const reservaRef = doc(db, "reservas", "XtcTzT0MIQV2YYqtMcto")
+    //     getDoc(reservaRef).then(snapshot =>{
+    //         if(snapshot.exists()){
+    //             console.log(snapshot.id)
+    //             console.log(snapshot.data())
+    //             console.log({id: snapshot.id, ...snapshot.data()})
+    //         }
+    //     })
+
+        // const reservasRef = collection(db, "reservas")
+        // getDocs(reservasRef).then(snapshots => {
+        //     setItems(snapshots.docs.map(doc => ({id: doc.id, ...doc.data()})))
+        // })
+    // },[])
+
+//         const reservasRef = collection(db,"reservas")
+//         getDocs(reservasRef).then(snapshots => {
+//             if (snapshots.size === 0){
+//                 console.log ("No hay products");
+//             }
+//             SVGMetadataElement(snapshots.docs.map(doc => ({id: doc.id, ...doc.data()})));
+//         });
+    
+// },[]);
 
     
     return (
--- src/index.js
+++ src/index.js
@@ -6,12 +6,32 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './components/Navbar' ;
 // import ' ./styles/styles.css' 
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDz3Ak07lt6312DzmzTKUcNAT-Ego8eNdA",
+  authDomain: "primer-react-e7dea.firebaseapp.com",
+  projectId: "primer-react-e7dea",
+  storageBucket: "primer-react-e7dea.appspot.com",
+  messagingSenderId: "647988935559",
+  appId: "1:647988935559:web:f8d02b467f48dacfcb7122"
+};
+
+// Initialize Firebase
+ initializeApp(firebaseConfig);
+
+
 const root = ReactDOM.createRoot(document.getElementById('root' ));
 root.render(
     <React.StrictMode>
         <App />
     </React.StrictMode>
 );
+
 
 
 
